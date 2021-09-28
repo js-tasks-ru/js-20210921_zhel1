9c678772b4a8adc95c021363b79079620fcf9e46
Denis Zheleznov <zhel1@yandex.ru>
2021-09-28 19:01:11 +0300

    2d lesson, task 1

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,12 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+    if (param === 'asc') {
+        return arr.concat().sort((a, b) => a.localeCompare(b, 'ru-RUen-GB', {sensitivity: 'variant', caseFirst: 'upper'}));
+    } else if (param === 'desc') {
+        return arr.concat().sort((a, b) => b.localeCompare(a, 'ru-RUen-GB', {sensitivity: 'variant', caseFirst: 'upper'}));
+    } else {
+        console.log('param is incorrect');
+        return arr.concat();
+    }
 }

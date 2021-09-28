61a0f4e5690fe96d6b5f4800a7576bca85969be3
Denis Zheleznov <zhel1@yandex.ru>
2021-09-28 19:35:19 +0300

    2d lesson, task 2

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,12 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
-
+    let newObj = { };
+    for (let i = 0; i < fields.length; i++) {
+        if (fields[i] in obj) {
+            newObj[fields[i]] = obj[fields[i]];
+        }
+    }
+}
+    return newObj;
 };

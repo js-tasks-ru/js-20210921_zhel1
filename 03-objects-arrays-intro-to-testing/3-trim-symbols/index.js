e3ee49921c00639b186083307a5aa5ce6cffbe86
Denis Zheleznov <zhel1@yandex.ru>
2021-10-03 13:03:09 +0300

    3d lesson, task 3

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,28 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+	if (size === 0)
+		return '';
 
+	if (size === undefined)
+		return string;
+
+	let currentCount = 0;
+	let previousChar;
+
+	for (let i = 0; i < string.length + 1; i++) {
+		let char = string[i];
+		if (char === previousChar || previousChar === undefined) {
+			currentCount++;
+		} else {
+			if (currentCount > size) {
+				string = string.replace(previousChar.repeat(currentCount), previousChar.repeat(size));
+				i = i - (currentCount - size);
+			}
+			currentCount = 1;
+		}
+		previousChar = char;
+	}
+
+	return string;
 }
